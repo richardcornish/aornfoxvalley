e34fd65d7fba9d0d71c20cfca81509019f26dd34
Richard Cornish <richardcornish@gmail.com>
2014-05-09 15:00:34 -0500

    Fixed admin password reset for Django 1.6

--- aornfoxvalley/urls.py
+++ aornfoxvalley/urls.py
@@ -5,13 +5,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 
-    # Admin
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-
-    # Robots
-    url(r'^robots\.txt$', include('robots.urls')),
-
     # AORN
     url(r'^news/', include('aornfoxvalley.news.urls')),
     url(r'^events/', include('aornfoxvalley.events.urls')),
@@ -20,6 +13,20 @@ urlpatterns = patterns('',
     url(r'^feeds/', include('aornfoxvalley.feeds.urls')),
     url(r'^', include('aornfoxvalley.sitemaps.urls')),
     url(r'^$', 'aornfoxvalley.views.home', name='home'),
+
+    # Admin password reset
+    url(r'^admin/password_reset/$', 'django.contrib.auth.views.password_reset', name='admin_password_reset'),
+    url(r'^admin/password_reset/done/$', 'django.contrib.auth.views.password_reset_done', name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm', name='password_reset_confirm'),
+    url(r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete', name='password_reset_complete'),
+
+    # Admin
+    url(r'^admin/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/admin/'}),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+
+    # Robots
+    url(r'^robots\.txt$', include('robots.urls')),
 
 )
 
